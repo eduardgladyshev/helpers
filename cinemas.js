da3297ebbe6c1d0cadc5c24cc4cf5863a2fe2030
eddi <eddiglad@gmail.com>
2018-06-08 17:44:52 +0300

    add mongo

--- cinemas.js
+++ cinemas.js
@@ -1,60 +1,92 @@
 let request  = require('superagent');
 let user     = require('./user.conf');
+let MongoClient = require('mongodb').MongoClient;
 require('console.table');
 
 let cinemas  = [];
 let cinemaList = [];
 
-let agent = request.agent();
 
-agent
-  .post(`https://kinoplan24.ru/login`)
-  .type('form')
-  .send({email: user.email, password: user.password})
-  .then(res => {
-    return agent.
-            get(`https://kinoplan24.ru/api/user/v2/0`);
-  })
-  .then(res => {
-    cinemas = res.body.cinema;
+const url = 'mongodb://10.30.0.8:27017';
 
-    cinemas.forEach(i => {
-      cinemaList.push({'Название': i.title.ru, id: i.id, node: i.dd24_pro});
-    });
+
+const dbName = 'kinoplan';
+
+
+MongoClient.connect(url, function(err, client) {
+  if(err) throw err; 
+  console.log("Connected successfully to mongo");
+
+  const db = client.db(dbName);
+
+  let cinemaCollection = db.collection('cinema');
+  cinemaCollection.find({'owner': })
+
+  client.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let agent = request.agent();
+
+// agent
+//   .post(`https://kinoplan24.ru/login`)
+//   .type('form')
+//   .send({email: user.email, password: user.password})
+//   .then(res => {
+//     return agent.
+//             get(`https://kinoplan24.ru/api/user/v2/0`);
+//   })
+//   .then(res => {
+//     cinemas = res.body.cinema;
+
+//     cinemas.forEach(i => {
+//       cinemaList.push({'Название': i.title.ru, id: i.id, node: i.dd24_pro});
+//     });
     
-    console.table('Список кинотеатров', cinemaList);
+//     console.table('Список кинотеатров', cinemaList);
 
-    handleCinemaId();
+//     handleCinemaId();
 
-  })
-  .catch(err => console.log('err', err));
+//   })
+//   .catch(err => console.log('err', err));
   
-function handleCinemaId(){
+// function handleCinemaId(){
 
-  process.stdin.on('readable', () => {
-    let cinemaId = process.stdin.read();
+//   process.stdin.on('readable', () => {
+//     let cinemaId = process.stdin.read();
 
-    if(!+cinemaId && cinemaId !== null) {
-      console.table('Список кинотеатров', cinemaList);
-    }
+//     if(!+cinemaId && cinemaId !== null) {
+//       console.table('Список кинотеатров', cinemaList);
+//     }
 
-    cinemas.forEach(i => {
-      let halls = []; 
+//     cinemas.forEach(i => {
+//       let halls = []; 
 
-      if(i.id == cinemaId){
-        i.halls.forEach(i => {
-          halls.push({'Номер': i.title, id: i.id});
-        });
+//       if(i.id == cinemaId){
+//         i.halls.forEach(i => {
+//           halls.push({'Номер': i.title, id: i.id});
+//         });
 
-        console.table('Список залов', halls);
-      } 
-    });
+//         console.table('Список залов', halls);
+//       } 
+//     });
 
-    process.stdout.write(`Id кинотеатра: `);
-  });
+//     process.stdout.write(`Id кинотеатра: `);
+//   });
 
-  process.stdin.on('end', () => {
-    process.stdout.write('end');
-  });
-}
+//   process.stdin.on('end', () => {
+//     process.stdout.write('end');
+//   });
+// }
 
